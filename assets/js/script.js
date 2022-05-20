5094ed089d7872bddfc165d71eec1240e5e0cb81
Martin Krook <krookmartin@yahoo.com>
2022-05-20 13:43:28 +0000

    add playeGuess function and images update function

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,17 +35,43 @@ function buildKeyboard() {
    */
   function gameWord() {
     currentWord = answer.split('').map(letter => (guess.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-  
     document.getElementById('currentTeam').innerHTML = currentWord;
     console.log(answer);
   } 
-
+  function updateGallowImg() {
+    if(mistakes === 6){
+    document.getElementById('gallowImage').src = 'assets/images/hangman0.JPG';
+  }
+  else if (mistakes === 5)
+  document.getElementById('gallowImage').src = 'assets/images/hangman1.JPG';
+  
+  else if (mistakes === 4){
+  document.getElementById('gallowImage').src = 'assets/images/hangman2.JPG';
+  }
+  else if (mistakes === 3){
+    document.getElementById('gallowImage').src = 'assets/images/hangman3.JPG';
+  }
+  else if (mistakes === 2){
+    document.getElementById('gallowImage').src = 'assets/images/hangman4.JPG';
+  }
+  else if (mistakes === 1){
+    document.getElementById('gallowImage').src = 'assets/images/hangman5.JPG';
+  }
+  else if (mistakes === 0) 
+    document.getElementById('gallowImage').src = 'assets/images/hangman6.JPG';
+  }
+  
   function correctWord() {
     if(currentWord === answer) {
-      document.getElementById('keyboard').innerHTML = 'Winner!';
+      document.getElementById('currentTeam').innerHTML = 'Winner!';
     } 
   }
+  function gameOver() {
+    if(mistakes === 0){
+      document.getElementById('currentTeam').innerHTML = 'Game over!';
+    }
 
+  }
   function upDateChancesLeft () {
     document.getElementById('mistakes').innerHTML = mistakes;
   }
@@ -59,9 +85,9 @@ function buildKeyboard() {
       correctWord();
     } else if (answer.indexOf(chosenLetter) === -1) {
       mistakes--;
-      updateMistakes();
-      //checkIfGameLost();
-      //updateHangmanPicture();
+      upDateChancesLeft();
+      updateGallowImg();
+      gameOver();
     }
   }
   
@@ -76,7 +102,16 @@ function buildKeyboard() {
       'St Marys Stadium', 'Tottenham Hotspur Stadium', 'Vicarage Road', 'London Stadium', 'Molineux Stadium'];
     let hintIndex = premierTeams.indexOf(answer);
     window.alert (hints [hintIndex]); 
-
+}
+document.getElementById('reset').addEventListener('click', resetButton);
+  function resetButton () {
+    mistakes = 6;
+    guess = [];
+    document.getElementById('gallowImage').src = 'assets/images/hangman0.JPG';
+    getRandomTeam();
+    buildKeyboard();
+    gameWord(); 
+    playerGuess();
   }
   
   getRandomTeam();
